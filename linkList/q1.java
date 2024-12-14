54b13d353572e5d79c6ec3dc700e18f56b1511b4
rajshubham56 <137806378+rajshubham56@users.noreply.github.com>
2024-12-14 18:49:10 +0530

    testing to

--- linkList/q1.java
+++ linkList/q1.java
@@ -1,6 +1,21 @@
 package linkList;
 
+import java.util.Scanner;
+
 public class q1 {
-    // hello world
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter n :");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.print("enter all element : ");
+        for(int i=0;i<nums.length;i++){
+            nums[i] = sc.nextInt();
+        }
+        Quick_Sort(nums,0,n-1);
+         for(int i=0;i<nums.length;i++){
+             System.out.print(nums[i]+" ");
+        }
+    }
+
 }
