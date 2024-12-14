78359a60bfa1fc6f666986b7601e206fcd58e852
rajshubham56 <137806378+rajshubham56@users.noreply.github.com>
2024-12-14 23:39:52 +0530

    code improve

--- binary/binaryq1.java
+++ binary/binaryq1.java
@@ -1,8 +1,21 @@
-package binary;
+
 
 import java.util.Scanner;
 
 public class binaryq1 {
+    static boolean Quick_Sort(int[] nums,int st,int end,int target){
+        if(st<end){
+            int mid = (st+end)/2;
+        if(nums[mid]==target) return true;
+        else if(nums[mid]>target){
+            return Quick_Sort(nums,st,mid-1,target);
+        }else{
+            return Quick_Sort(nums,mid+1,end,target);
+        }  
+        } 
+        return false;
+        
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter n :");
@@ -12,9 +25,10 @@ public class binaryq1 {
         for(int i=0;i<nums.length;i++){
             nums[i] = sc.nextInt();
         }
-        Quick_Sort(nums,0,n-1);
-         for(int i=0;i<nums.length;i++){
-             System.out.print(nums[i]+" ");
-        }
+        System.out.print("enter target :");
+        int target = sc.nextInt();
+
+        boolean ans = Quick_Sort(nums,0,n-1,target);
+        System.out.print(ans);
     }
 }
